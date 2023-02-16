3824025fa29d79d6e651fec7bb9de7e430879735
Solz-K <122835592+Solz-K@users.noreply.github.com>
2023-02-16 15:43:29 -0800

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - C programme
+ * main - Entry point
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
-
+	puts("\"Programming is like building a multilingual puzzle\n")
 	return (0);
 }
