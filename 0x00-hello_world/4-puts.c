8354a25df1cf32be4a5b02ab31190713c45a6c59
Solz-K <122835592+Solz-K@users.noreply.github.com>
2023-02-16 15:31:21 -0800

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - C programme
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	char Mystr[] = "Programming is like building a multilingual puzzle";
-	puts(Mystr);
+	puts("\"Programming is like building a multilingual puzzle\n");
 
 	return (0);
 }
