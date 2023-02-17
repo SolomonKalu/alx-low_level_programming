b2c091c0e8ab93d2683d26571ead3f58093fbdba
Solz-K <122835592+Solz-K@users.noreply.github.com>
2023-02-17 04:41:53 -0800

    Updated Readme

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -8,6 +8,6 @@
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	printf("with proper grammar, but the outcome is a piece of art, \n");
 	return (0);
 }
--- 0x00-hello_world/README.md
+++ 0x00-hello_world/README.md
@@ -1,1 +1,3 @@
 Project 0. A script that runs a C file through the preprocessor and save the result into another file.
+
+Projetc 1. A script that compiles a C file but does not link.
