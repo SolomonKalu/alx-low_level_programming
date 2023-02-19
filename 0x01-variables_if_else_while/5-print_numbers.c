18a24a0691c1c9453ed4d4bbccdbdc3ae5f49145
Solz-K <122835592+Solz-K@users.noreply.github.com>
2023-02-19 13:52:06 -0800

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - prints single digit numbers
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		printf("%d", num);
+	printf("\n");
+	return (0);
+}
