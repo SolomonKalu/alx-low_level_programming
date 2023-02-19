a57fa1e614747fbe2fcae7551d09309955e0b6b3
Solz-K <122835592+Solz-K@users.noreply.github.com>
2023-02-19 14:11:35 -0800

    PPP

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - possible combinations of sinngle-digit nums
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 48; num < 58; num++)
+	{
+		putchar(num);
+		if (num != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
