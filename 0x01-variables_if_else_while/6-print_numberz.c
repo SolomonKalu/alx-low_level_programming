bdca003c657d033189718e75cc7d6edc24575c65
Solz-K <122835592+Solz-K@users.noreply.github.com>
2023-02-19 13:56:48 -0800

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - single digit nums of base 10
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 32; num < 42; num++)
+	{
+		putchar(num);
+	}
+	putchar('\n');
+	return (0);
+}
