c0b99a697eecbf0c6c883799b3b2a539e7937f7f
Solz-K <122835592+Solz-K@users.noreply.github.com>
2023-02-22 01:32:00 -0800

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+  * print_alphabet - prints the alphabet, in lowercase, and a new line
+  */
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+	_putchar(c);
+	}
+	_putchar('\n');
+}
