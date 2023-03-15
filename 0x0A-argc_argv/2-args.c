b84839c6ba54bf59b4e81813cd3e885121f5a2f1
Solz-K <122835592+Solz-K@users.noreply.github.com>
2023-03-15 03:12:02 -0700

    Args

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * main - it all starts here
+ * @argc: the number of arguments
+ * @argv: array of poointers to arguments
+ *
+ * Return: Always 0.
+ */
+
 int main(int argc, char *argv[])
 {
 	int x;
