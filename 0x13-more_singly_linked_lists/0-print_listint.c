1ab49bcb1cd79ac1c07c1414f6af13d2c3b6d5f0
Solz-K <122835592+Solz-K@users.noreply.github.com>
2023-04-04 06:11:41 -0700

    Prints all the elements

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -5,13 +5,13 @@
  * print_listint -  prints elements of a listint_t list
  * @h: points to the initial node
  *
- * return: list size
+ * Return: size of list
  */
 
 size_t print_listint(const listint_t *h)
 {
 	size_t val = 0;
-	
+
 	while (h)
 	{
 		printf("%d\n", h->n);
