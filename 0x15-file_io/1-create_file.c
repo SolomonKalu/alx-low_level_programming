ebf9b510ed6c5216096ea4643dd633527d00fc05
Solz-K <122835592+Solz-K@users.noreply.github.com>
2023-04-11 05:23:45 -0700

    Creates file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * create_file - function that creates a file
+ * @filename: the name of the file
+ * @text_content: is a NULL terminated string to write to the file
+ *
+ * Return: 1 on success, -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd, nl, flw;
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if (filename == NULL)
+		return (-1);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content)
+	{
+		for (nl = 0; text_content[nl];)
+			nl++;
+	}
+
+	flw = write(fd, text_content, nl);
+
+	if (flw == -1)
+		return (-1);
+
+	close(fd);
+	return (1);
+}
