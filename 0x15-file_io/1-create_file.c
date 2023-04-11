5cd9fa073fa84d2d08232c230d9bee28f1600ea0
Solz-K <122835592+Solz-K@users.noreply.github.com>
2023-04-11 05:50:28 -0700

    Creates File

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,7 +16,9 @@ int create_file(const char *filename, char *text_content)
 
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
-	if (!filename || fd == -1)
+	if (!filename)
+		return (-1);
+	if (fd == -1)
 		return (-1);
 
 	if (text_content)
