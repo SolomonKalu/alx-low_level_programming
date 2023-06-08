63e6ffc951055bfb3c6448eb3455848ab8e627f9
SolomonKalu <122835592+SolomonKalu@users.noreply.github.com>
2023-06-08 08:24:41 -0700

    square root recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -3,7 +3,7 @@
 
 /**
  * for_sqrt - to find the root of the number
- * @number: number
+ * @n: number to locate root
  * @r: root
  *
  * Return: the root, if false -1
@@ -11,28 +11,26 @@
 
 int for_sqrt(int n, int r)
 {
-        if ((r * r) == n)
-        {
-                return (r);
-        }
-        if (r * r > n)
-        {
-                return (-1);
-        }
-        return (for_sqrt(n, r + 1));
+	if ((r * r) == n)
+	{
+		return (r);
+	}
+	if (r * r > n)
+	{
+		return (-1);
+	}
+	return (for_sqrt(n, r + 1));
 }
 
 /**
  * _sqrt_recursion - function that returns the natural square root of a number
- * @n: number to return
+ * @n: number to return root
  *
  * return: if natural root return 1 if not return -1
  */
 
 int _sqrt_recursion(int n)
 {
-	int r = 0;
-
 	if (n < 0)
 	{
 		return (-1);
@@ -41,5 +39,5 @@ int _sqrt_recursion(int n)
 	{
 		return (1);
 	}
-	return (for_sqrt(n, r));
+	return (for_sqrt(n, 0));
 }
