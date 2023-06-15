5569922b0a7d79c4bd4d4d4cf78b2689a8b1e915
SolomonKalu <122835592+SolomonKalu@users.noreply.github.com>
2023-06-15 12:59:59 -0700

    Checked Malloc

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -4,14 +4,16 @@
 
 /**
  * malloc_checked - allocates memory using malloc
- * @b: unsigned int
+ * @b: byte
  *
- * Return: Nothing
+ * Return: pointer
  */
 
 void *malloc_checked(unsigned int b)
 {
-	void *ptr = malloc(b);
+	void *ptr;
+
+	ptr = malloc(b);
 
 	if (ptr == NULL)
 	{
