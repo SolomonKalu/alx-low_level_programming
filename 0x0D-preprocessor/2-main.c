6e62869a6a84ad55ccef789dcad6d172a5466a95
SolomonKalu <122835592+SolomonKalu@users.noreply.github.com>
2023-06-19 10:28:52 -0700

    Main

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - program name file and new line
+ *
+ * Return: Nothing
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
