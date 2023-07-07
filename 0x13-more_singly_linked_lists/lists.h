58a0ebbb798c01a80680b612789be389ad3deb4a
SolomonKalu <122835592+SolomonKalu@users.noreply.github.com>
2023-07-06 19:49:12 -0700

    Updated

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -31,5 +31,6 @@ int pop_listint(listint_t **head);
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
 int sum_listint(listint_t *head);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
+listint_t *reverse_listint(listint_t **head);
 
 #endif
