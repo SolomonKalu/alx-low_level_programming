4a487a8fb9d4ba3be9d302100bc403396c8edb0e
SolomonKalu <122835592+SolomonKalu@users.noreply.github.com>
2023-08-03 20:59:41 +0100

    Task getbit

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * get_bit - returns the value of a bit at a given index
+ * @n: value
+ * @index: is the index, starting from 0.
+ * 
+ * Return: value of bit at index, minus 1 if error
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index >= sizeof(n) * 8)
+	{
+		return (-1);
+	}
+	return (n >> index & 1);
+}
