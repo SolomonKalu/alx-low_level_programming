8a2b2203873e7f52762a62a78357fef534ad6cf8
SolomonKalu <122835592+SolomonKalu@users.noreply.github.com>
2023-08-03 18:28:39 +0100

    Task Binary

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,18 +8,17 @@
  
  unsigned int binary_to_uint(const char *b)
  {
-   unsigned int number = 0;
-   
-   if (b == 0)
-   {
-     return (0);
-   }
-   while (*b != '\0')
-   {
-     if (*b != '0' && *b != '1')
-     return (0);
-     number = (number << 1) | (*b - '0');
-     ++b;
-   }
-   return (number);
+  unsigned int number = 0;
+  if (b == 0)
+  {
+  return (0);
+  }
+  while (*b != '\0')
+  {
+  if (*b != '0' && *b != '1')
+  return (0);
+  number = (number << 1) | (*b - '0');
+  ++b;
+  }
+  return (number);
  }
