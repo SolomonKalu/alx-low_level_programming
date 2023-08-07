01bceb5bd97a8f37e8b3e07d713f89dce52e6489
SolomonKalu <122835592+SolomonKalu@users.noreply.github.com>
2023-08-07 12:31:27 -0700

    Task 0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * read_textfile - function to read a text file and print
+ * @letters: numbers to read and print
+ * @filename: name to read
+ *
+ * Return: number to read and print
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fp;
+	char *b;
+	ssize_t wr, rd;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+	fp = open(filename, O_RDONLY);
+	if (fp == -1)
+	{
+		return (0);
+	}
+	b = malloc(sizeof(char) * letters);
+	if (b == NULL)
+	{
+		return (0);
+	}
+	b = malloc(sizeof(char) * letters);
+	if (b == NULL)
+	{
+		return (0);
+	}
+	rd = read(fp, b, letters);
+	wr = write(STDOUT_FILENO, b, rd);
+	close(fp);
+	free(b);
+	return (wr);
+}
