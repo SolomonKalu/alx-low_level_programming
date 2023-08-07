3571ea2a4afba14d2f9194f29fca8628b2d2fd3d
SolomonKalu <122835592+SolomonKalu@users.noreply.github.com>
2023-08-07 13:45:32 -0700

    Header File

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,6 +5,8 @@
 #include <stdlib.h>
 #include <stddef.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <fcntl.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
