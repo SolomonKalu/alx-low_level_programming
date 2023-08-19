ca987e22136d77ad9392cf3ee496b4859c8597ff
SolomonKalu <122835592+SolomonKalu@users.noreply.github.com>
2023-08-19 05:20:51 +0100

    Error fix

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-def element_at(my_list, idx):
-    for x in my_list:
-        if idx == my_list.index(x):
-            return(x)
-        elif idx > len(my_list):
-            return None
-        elif idx < 0:
-            return None
