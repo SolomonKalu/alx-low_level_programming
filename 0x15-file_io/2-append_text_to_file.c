4111a355e4c26fc6597050f3fb3439689fd5ba01
SolomonKalu <122835592+SolomonKalu@users.noreply.github.com>
2023-11-30 22:13:03 -0800

    AppendTextToFile

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -25,7 +25,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	}
 	if (text_content == NULL)
 	{
-		while (text_content[nl]; ++nl)
+		for (nl = 0; text_content[nl]; ++nl)
 			;
 		rdwr = write(file, text_content, nl);
 		if (rdwr == -1)
